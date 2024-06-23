48a5553864538847a0a7c93bd02cff5039c233ef
JQ4386 <jingqi_qu@berkeley.edu>
2024-06-22 19:58:27 -0700

    add gitignore

---
+++ server/Ingest-to-TextDump/Pipeline-part1.py
@@ -0,0 +1,6 @@
+# extract text from a PDF file
+# input: PDF file
+# output: text file
+# usage: python Pipeline-part1.py <input PDF file> <output text file>
+# example: python Pipeline-part1.py /mnt/data/Ingest-to-TextDump/0000000001.pdf /mnt/data/Ingest-to-TextDump/0000000001.txt
+
